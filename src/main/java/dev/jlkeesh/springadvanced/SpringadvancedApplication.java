c358e7b49c38e60d07039ffcfe0b812e2cc4562e
pdp <john.lgd65@gmail.com>
2023-04-18 16:58:48 +0500

    initial

--- src/main/java/dev/jlkeesh/springadvanced/SpringadvancedApplication.java
+++ src/main/java/dev/jlkeesh/springadvanced/SpringadvancedApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.zalando.logbook.Logbook;
 
 import java.time.Duration;
 
