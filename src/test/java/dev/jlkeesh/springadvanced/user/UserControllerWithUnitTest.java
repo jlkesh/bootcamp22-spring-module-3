ae1c6f7b5e2dda55648c6a7bb47fe279a2a09515
pdp <john.lgd65@gmail.com>
2023-04-29 16:17:48 +0500

    initial

--- src/test/java/dev/jlkeesh/springadvanced/user/UserControllerWithUnitTest.java
+++ src/test/java/dev/jlkeesh/springadvanced/user/UserControllerWithUnitTest.java
@@ -3,24 +3,27 @@ package dev.jlkeesh.springadvanced.user;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 /*@ExtendWith(MockitoExtension.class)*/
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest
 class UserControllerWithUnitTest {
 
     @Autowired
@@ -49,19 +52,78 @@ class UserControllerWithUnitTest {
 
     @Test
     void testCreateApiForSuccessfulResponse() throws Exception {
+        when(userService.create(any())).thenReturn(User.builder().id(1).build());
+        User user = User.builder()
+                /*.email("choy@mail.ru")*/
+                .otp("123")
+                .password("123")
+                .username("choy")
+                .build();
+
+        mockMvc.perform(post("/api/users")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(user))
+        ).andExpect(status().isCreated());
+        verify(userService, times(1)).create(any());
+    }
+
+    @Test
+    void testCreateApiForBadRequestResponse() throws Exception {
+        User user = User.builder()
+                /*.email("choy@mail.ru")*/
+                .otp("123")
+                .password("123")
+                .username("choy")
+                .build();
+        when(userService.create(any())).thenReturn(User.builder().id(1).build());
+
+        mockMvc.perform(post("/api/users")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(user))
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testCreateApiForSuccessAndCheckResponse() throws Exception {
         User user = User.builder()
                 .email("choy@mail.ru")
                 .otp("123")
                 .password("123")
                 .username("choy")
                 .build();
+        when(userService.create(any())).thenReturn(User.builder().id(1).build());
+
+
         mockMvc.perform(post("/api/users")
                         .contentType("application/json")
-                        .content(objectMapper.writeValueAsString(user)))
+                        .content(objectMapper.writeValueAsString(user))
+                )
+                .andExpect(jsonPath("$.id").hasJsonPath())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.email").hasJsonPath())
+
                 .andExpect(status().isCreated());
-        verify(userService, times(1)).create(any());
+    }
 
 
+    @Test
+    void testCreateApiForSuccessAndCheckResponse2() throws Exception {
+        User user = User.builder()
+                .email("choy@mail.ru")
+                .otp("123")
+                .password("123")
+                .username("choy")
+                .build();
+        when(userService.create(any())).thenReturn(User.builder().id(1).build());
+        MvcResult mvcResult = mockMvc.perform(post("/api/users")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(user))
+                ).andExpect(status().isCreated())
+                .andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String contentAsString = response.getContentAsString();
+        User user1 = objectMapper.readValue(contentAsString, User.class);
+        assertEquals(1, user1.getId());
     }
 
     @Test
